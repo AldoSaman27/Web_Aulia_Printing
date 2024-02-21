f8735065d9832c822d914de86de78d87943b4ef7
Aldo Saman <aldosaman711@gmail.com>
2024-02-21 19:20:42 +0700

    feat: added About page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './Pages/About';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
             </Routes>
         </Router>
     );
