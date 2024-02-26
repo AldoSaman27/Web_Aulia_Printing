c8848eb73f23234109baa30a6e722b053b679ab2
Aldo Saman <aldosaman711@gmail.com>
2024-02-27 01:02:06 +0700

    ops: Delete About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Pages/Home';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from './Pages/About';
-import Product from './Pages/Product';
-import Contact from './Pages/Contact';
+import Home from "./Pages/Home";
+import Product from "./Pages/Product";
+import Contact from "./Pages/Contact";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/product' element={<Product/>}/>
-                <Route path='/contact' element={<Contact/>}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
