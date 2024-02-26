69a6eb466b797ed5a67af7712524742020fc5a96
Aldo Saman <aldosaman711@gmail.com>
2024-02-27 00:32:16 +0700

    refactor: Changed Component Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,108 +1,70 @@
-import { Container } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import { Instagram, Linkedin, Tiktok, Youtube } from "react-bootstrap-icons";
 
 const Footer = () => {
-  const navigate = useNavigate();
-
   return (
-    <footer>
-      <Container>
-        <div className="row">
-          <div className="col-12 col-md-6 d-flex flex-column align-items-center align-items-md-start justify-content-center mb-4">
-            <h1 className="m-0 p-0 text-white">Aulia Printing</h1>
-            <p className="m-0 p-0 text-secondary fw-semibold">
-              Percetakan | Service Komputer / Laptop & Printer | Fotocopy & ATK
-            </p>
-          </div>
-          <div className="col-12 col-md-6 d-flex gap-3 align-items-center justify-content-center justify-content-md-end mb-4">
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="Sosmed d-flex align-items-center justify-content-center rounded-circle"
-            >
-              <Linkedin size={15} color="black" />
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="Sosmed d-flex align-items-center justify-content-center rounded-circle"
-            >
-              <Instagram size={15} color="black" />
-            </a>
-            <a
-              href="https://tiktok.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="Sosmed d-flex align-items-center justify-content-center rounded-circle"
-            >
-              <Tiktok size={15} color="black" />
-            </a>
-            <a
-              href="https://youtube.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="Sosmed d-flex align-items-center justify-content-center rounded-circle"
-            >
-              <Youtube size={15} color="black" />
-            </a>
-          </div>
+    <footer className="footer">
+      <div className="row">
+        <div className="col-12 col-md-6 d-flex flex-column align-items-center align-items-md-start justify-content-center mb-4">
+          <h1 className="footer-brand">Aulia Printing</h1>
+          <p className="footer-description">
+            Percetakan | Service Komputer / Laptop & Printer | Fotocopy & ATK
+          </p>
         </div>
-        <div className="d-flex align-items-center justify-content-center justify-content-md-start mb-4">
-          <ul className="p-0 m-0 d-flex gap-4">
-            <li
-              style={{
-                listStyle: "none",
-                fontSize: "1.1rem",
-                cursor: "pointer",
-              }}
-              className="text-white fw-semibold"
-              onClick={() => navigate("/")}
-            >
-              Home
-            </li>
-            <li
-              style={{
-                listStyle: "none",
-                fontSize: "1.1rem",
-                cursor: "pointer",
-              }}
-              className="text-white fw-semibold"
-              onClick={() => navigate("/about")}
-            >
-              About Us
-            </li>
-            <li
-              style={{
-                listStyle: "none",
-                fontSize: "1.1rem",
-                cursor: "pointer",
-              }}
-              className="text-white fw-semibold"
-              onClick={() => navigate("/product")}
-            >
-              Product
-            </li>
-            <li
-              style={{
-                listStyle: "none",
-                fontSize: "1.1rem",
-                cursor: "pointer",
-              }}
-              className="text-white fw-semibold"
-              onClick={() => navigate("/contact")}
-            >
-              Contact
-            </li>
-          </ul>
+        <div className="col-12 col-md-6 d-flex gap-3 align-items-center justify-content-center justify-content-md-end mb-4">
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-sosmed"
+          >
+            <Linkedin size={15} color="black" />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-sosmed"
+          >
+            <Instagram size={15} color="black" />
+          </a>
+          <a
+            href="https://tiktok.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-sosmed"
+          >
+            <Tiktok size={15} color="black" />
+          </a>
+          <a
+            href="https://youtube.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="footer-sosmed"
+          >
+            <Youtube size={15} color="black" />
+          </a>
         </div>
-        <hr color="white" style={{ height: 3 }} />
-        <p className="m-0 p-0 text-white text-center text-md-end">
-          © {new Date().getFullYear()} Aulia Printing
-        </p>
-      </Container>
+      </div>
+      <div className="d-flex align-items-center justify-content-center justify-content-md-start mb-4">
+        <div className="footer-menu">
+          <a href="#home" className="footer-link">
+            Home
+          </a>
+          <a href="#about-us" className="footer-link">
+            About Us
+          </a>
+          <a href="#product" className="footer-link">
+            Product
+          </a>
+          <a href="#contact" className="footer-link">
+            Contact
+          </a>
+        </div>
+      </div>
+      <hr color="white" style={{ height: 3 }} />
+      <p className="footer-copyright">
+        © {new Date().getFullYear()} Aulia Printing
+      </p>
     </footer>
   );
 };
